b899888d5c55bba94e365772c2cae0895b3a81d3
Joe Rrobin <joerobin26@gmail.com>
2024-10-03 13:04:58 +0530

    URL class more stuff added

--- url.py
+++ url.py
@@ -1,4 +1,26 @@
+import socket
 class URL:
+    def request(self):
+        s = socket.socket(
+            family=socket.AF_INET,
+            type=socket.SOCK_STREAM,
+            proto=socket.IPPROTO_TCP,
+        )
+
+        s.connect((self.host,80))
+
+        request = "GET {} HTTP/1.0\r\n".format(self.path)
+        request += "Host: {}\r\n".format(self.host)
+        request += "\r\n"
+        
+
     def __init__(self,url):
-        self.scheme, url = url.split("://",1)
-        assert self.
+        # seperate the scheme from the url and check if it is "http"
+        self.scheme, url = url.split("://", 1)
+        assert self.scheme == "http" # throws error if false
+        # seperate the path from the host  if there is no path then the path will be just /
+        if "/" not in url:
+            url = url + "/"
+        self.host, url = url.split("/", 1)
+        self.path = "/" +  url
+
